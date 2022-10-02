ae7e6a85222f9de39c549c8a589e0d48b153c938
lgaid <104643045+lgaid@users.noreply.github.com>
2022-10-02 18:52:23 -0400

    Entity

--- src/main/java/com/cydeo/entity/BaseEntity.java
+++ src/main/java/com/cydeo/entity/BaseEntity.java
@@ -1,7 +1,15 @@
 package com.cydeo.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class BaseEntity {
 
 
