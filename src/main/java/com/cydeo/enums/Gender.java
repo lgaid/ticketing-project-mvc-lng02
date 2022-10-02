bb7a28699ef9c0eb6e8c13205d5f5c23bf58f909
lgaid <104643045+lgaid@users.noreply.github.com>
2022-10-02 19:00:04 -0400

    Enums

--- src/main/java/com/cydeo/enums/Gender.java
+++ src/main/java/com/cydeo/enums/Gender.java
@@ -4,5 +4,15 @@ public enum Gender {
 
     MALE("Male"),FEMALE("Female");
 
+    private String value;
+
+    Gender(String value){
+        this.value = value;
+    }
+
+    public String getValue(){
+        return value;
+    }
+
 
 }
